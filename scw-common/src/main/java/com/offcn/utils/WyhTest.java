f19a5da9434e34645343512d6a76ca52d24d3f9d
Administrator <3183606845@qq.com>
2021-03-05 19:32:18 +0800

    已经修改完

--- scw-common/src/main/java/com/offcn/utils/WyhTest.java
+++ scw-common/src/main/java/com/offcn/utils/WyhTest.java
@@ -3,5 +3,6 @@ package com.offcn.utils;
 public class WyhTest {
     public static void numberformat(String s){
         System.out.println(Integer.parseInt(s));
+        System.out.println("我已经更新了代码");
     }
 }
